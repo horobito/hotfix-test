fcd1fabfee7558bb20f2c63f74ac1524db4848f3
odkfu <odkfu1@gmail.com>
2023-04-23 22:42:16 +0900

    feat: set init

---
+++ src/main/java/com/example/hotfixtest2/HotfixTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.hotfixtest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotfixTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotfixTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hotfixtest2/HotfixTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotfixtest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotfixTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
